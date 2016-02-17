c085dfee9c3a2efd0aba6739c8d201e81d75caa3
Kris Simon <kris@ausdertechnik.de>
2016-02-17 18:17:51 +0100

    Initial Commit

---
+++ SKBugExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SKBugExample
+//
+//  Created by Kris Simon on 17/02/16.
+//  Copyright © 2016 aus der Technik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SKBugExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SKBugExample
+//
+//  Created by Kris Simon on 17/02/16.
+//  Copyright © 2016 aus der Technik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
